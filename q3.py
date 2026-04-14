0f6776a146390b14b930a9d57f718195b964601d
FluffyGiraffe09 <oroy2028@kehillahstudent.org>
2026-04-14 10:23:45 -0700

    dog classes

--- q3.py
+++ q3.py
@@ -3,9 +3,22 @@ class Cat:
     def __init__(self, name):
         self.name = name
     def __str__(self):
-        return f"a Cat names {self.name}"
+        return f"a Cat named {self.name}"
     def speak(self):
-        print(f"{sefl.name} says meow!")
+        print(f"{self.name} says meow!")
 
 ella = Cat("Ella")
 ella.speak()
+
+class Dog:
+    """a dog"""
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return f"A Dog named {self.name}"
+    def speak(self):
+        print(f"{self.name} says meow!")
+
+laila = Dog("Laila")
+laila.speak()
+print(laila)
